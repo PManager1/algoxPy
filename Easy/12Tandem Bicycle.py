deb8060a5addbc0cb8fc781ed41bff97cb6274b4
Jay <jpca999@gmail.com>
2023-06-06 15:35:57 -0400

    -

---
+++ Easy/12Tandem Bicycle.py
@@ -0,0 +1,11 @@
+def findFastest(red,blue):
+    print(red)
+    print(blue)
+
+
+
+
+redShirtSpeeds = [5,5,3,9,2];
+blueShirtSpeeds = [3,6,7,2,1];
+fastest = True;
+findFastest(redShirtSpeeds,blueShirtSpeeds)
---
+++ Easy/19_InsertionSort.py
@@ -0,0 +1,20 @@
+# 239PM
+def insertionSort(ar):
+    print(ar)
+    for j in range(len(ar)):
+        print("j =" , j, ar[j], "len =" , len(ar))
+        for i in range(j):
+            # here we do the swaping
+            print("j in=" , j)
+            print("8 ar[j] =",  ar[j])
+
+def swap(arr, p1,p2):
+    temp = p1
+    p1 = p2
+    p2=temp
+    return arr;
+
+
+
+ar = [8,5,2,9,5,6,3];
+insertionSort(ar)
