df0cb625053208705b566bea8abcb9f59fe5d274
Jay <jpca999@gmail.com>
2023-06-06 16:39:01 -0400

    -

--- Easy/19_InsertionSort.py
+++ Easy/19_InsertionSort.py
@@ -1,19 +1,20 @@
 # 239PM
+# [2, 5, 5, 8, 6, 9, 3]
 def insertionSort(ar):
     print(ar)
-    for j in range(len(ar)):
+    for j in range(1, len(ar)):
         print("j =" , j, ar[j], "len =" , len(ar))
         for i in range(j):
             # here we do the swaping
-            print("j in=" , j)
-            print("8 ar[j] =",  ar[j])
+            print("8-> ar =", ar, "j =" , j, " i =", i , "ar[i] =", ar[i], "ar[i+1]= ", ar[i+1] )
+            if  ( ar[i-1] > ar[i] ):  swap ( ar, i-1,i )
+            print("12============-j =" , j)
+            print("13- ar[j] =",  ar[j])
 
 def swap(arr, p1,p2):
-    temp = p1
-    p1 = p2
-    p2=temp
+    arr[p1], arr[p2] = arr[p2], arr[p1]
+    print("swapped arr =", arr)
     return arr
-
 
 ar = [8,5,2,9,5,6,3];
 insertionSort(ar)
