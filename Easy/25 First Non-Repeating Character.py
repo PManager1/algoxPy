22548088a365b090272c76e2d60ae8abc624c88a
Jay <jpca999@gmail.com>
2023-06-06 22:47:04 -0400

    -

--- Easy/25 First Non-Repeating Character.py
+++ Easy/25 First Non-Repeating Character.py
@@ -1,7 +1,24 @@
+def FirstNonRepeating(A):
+    # Code here
+    list = []  #will keep track of all the items.  If repeated here, it will pop/ remove it.
+    # So only the chars that are non repeating.
+    mp = {}
+    ans = ''
 
-def GiveIndexOF_FirstNonRepating(st):
-    return st;
+    for ch in A:
+        if ch not in mp:  # new character visited first time
+            print("ch=", ch); print("list=", list)
+            list.append(ch) #inserting into the list.
+            print("mp1=", mp)
+            mp[ch] = 1
+            print("mp=", mp)
+        else:
+            # if Repeated character encountered
+            if ch in list:  #keeping track of items in the list/ array.
+                list.remove(ch)
 
+    return ans
 
-st ="abcdcaf"
-GiveIndexOF_FirstNonRepating(st)
+l = "abcdcaf"
+ans1 = FirstNonRepeating(l)
+print(ans1)
