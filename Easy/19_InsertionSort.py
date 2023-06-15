1ad04eaa7898dcfa383d674ec29bd618ce47455e
Jay <jpca999@gmail.com>
2023-06-15 17:26:11 -0400

    -

--- Easy/19_InsertionSort.py
+++ Easy/19_InsertionSort.py
@@ -1,22 +1,23 @@
 # 239PM
 # [2, 5, 5, 8, 6, 9, 3]
 # status : not working.
+
+def swap( ar, p1, p2 ):
+    print("6-swap ar, p1, p2 =",  ar, p1, p2)
+    # ar[p2], ar[p1] = ar[p1], ar[p2]
+    ar[p1], ar[p2] = ar[p2], ar[p1]
+    print("      swapped arr =", ar)
+    return ar
+
 def insertionSort(ar):
-    print(ar)
-    for j in range(1, len(ar)):
-        print("j =" , j, ar[j], "len =" , len(ar))
-        for i in range(j):
-            # here we do the swaping
-            print("9=== j =" , j)
-            print("10-> ar =", ar, "j =" , j, " i =", i , "ar[i-1]= ", ar[i], "ar[i] =", ar[i+1]  )
-            if  ( ar[i] > ar[i+1] ):  swap ( ar, i,i+1 )
+    print("12ar=", ar)
+    for i in range(1, len(ar)):
+        print("i =", i, "ar[j] =", ar[i],   "len =" , len(ar))
 
-            print("13- ar[j] =",  ar[j])
-
-def swap(arr, p1,p2):
-    arr[p1], arr[p2] = arr[p2], arr[p1]
-    print("swapped arr =", arr)
-    return arr
+        j = i;
+        while ( j>0  and  ar[j-1] > ar[j] ):
+          swap( ar, j-1, j )
+          j-=1
 
 ar = [8,5,2,9,5,6,3];
 insertionSort(ar)
--- Easy/23RunLengthEncoding.py
+++ Easy/23RunLengthEncoding.py
@@ -1,38 +1,56 @@
+# https://github.com/pinglu85/algoExpert/blob/main/Easy/run-length-encoding.md
+
 # Given a non-empty string, we are asked to write a function that is going to run-length-encode the input string
 # and return the encoded string.
-# Run-length encoding refers to replacing repetitive, consecutive data by a count and one copy of a repeated data.
+# Run-length encoding refers to   ***** replacing repetitive ****** , consecutive data by a count and one copy of a repeated data.
 # For instance, AAABB will be encoded as 3A2B
+
 
 # If a sequence contains more than 9 consecutive, identical characters, we first encode 9 characters,
 # then the remaining ones.
 # For instance, AAAAAAAAAA (10 As) will be encode as 9A1A.
 # 9A4A2B4C2D
+
+
+# > Eliminate the use of many If Else cases.  Use as less as possible.
+#
+# First figure out which condition is like default  i.e  is going to be obstructed So use that in the IF condition,  Not for every Fucking case.
+# > Figure the unique cases where a lot of stuff gets changed, Vs where nothing is being changed.
+#
+# st = "AAAAAAAAAAAAABBCCCCDD"
+# Here large case has same characters, so use that as default & if case only when the code breaks.
+#
+
 def  runLengthEncoding(st):
     # print(st)
 
-    currentCh = "";
-    currentMax=0;
-    chars = []
-    for i, val in enumerate(st):
-        print(i, val)
-        if(st[i] == st[i+1]):
-            print("equal = st[i]=", st[i], i , i+1)
-            currentCh = st[i];
-            currentMax +=1
-        else:
-            print("23-currentMax =", currentMax, "currentCh=", currentCh )
-            # print("24-list=", list)
-            list = str(currentMax) + currentCh
-            print("26-list=", list)
-            chars.append(list)
-            currentMax = 0;
-            currentCh= st[i+1]
-            print("30-chars=", chars)
 
+    currentLen=1;
+    chars = []
+    for i in range (1, len(st)):
+        currentCh = st[i];
+        prevCh = st[i-1];
+        print("equal = st[i]=",  st[i-1], st[i])
+
+        if(currentCh != prevCh or currentLen == 9):
+          chars.append(str(currentLen))
+          chars.append(prevCh)
+          print("29-chars=", chars)
+          currentLen = 0;
+
+        currentLen +=1
+    chars.append(str(currentLen))
+    chars.append(prevCh)
+    print("33-chars=", chars)
+    res = "".join(chars)
+    print("res=", res)
+    return res
+    # print("23-currentLen =", currentLen, "currentCh=", currentCh )
 
 # count the number of AAAA s
 # Do in notebook first
-
+# 13A 2B 4C 2D
+# 9A4A2B4C2D
 
 st = "AAAAAAAAAAAAABBCCCCDD"
 runLengthEncoding(st)
