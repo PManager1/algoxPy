65928dd30784522c2b8e6b7094b6f2be1c406753
Jay <jpca999@gmail.com>
2023-06-15 01:30:49 -0400

    -

---
+++ Medium/28 move elemment to end.py
@@ -0,0 +1,27 @@
+# You are given an array of integers and an integer. Write a function that moves all
+# instances of that integer in the array to the end of the array. Return the array.
+
+# The function should mutate the input array, (using same array,  )
+# i.e. the operation should be performed in place,
+# and doesn’t need to maintain the order of the integers.
+
+#input = [2,1,2,2,2,3,4,2]; // 2
+#out = [1,3,4,2,2,2,2,2];  we dont care abt the order of the other numbers, only the 2s shoudl be at the last.
+
+def swap(a,b):
+    pass
+
+
+def moveElement(ar,num):
+    Left, Right = 0, len(ar)
+    while Left< Right:
+        if ar(Left) == 2
+
+        start += 1
+        end -= 1
+
+
+
+
+ar =[2,1,2,2,2,3,4,2];
+moveElement(ar,2)
