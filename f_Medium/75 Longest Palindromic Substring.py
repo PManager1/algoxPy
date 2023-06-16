1e42bf84ee3293cd78056aa184923d4beceb291a
Jay <jpca999@gmail.com>
2023-06-16 02:18:19 -0400

    -

--- f_Medium/75 Longest Palindromic Substring.py
+++ f_Medium/75 Longest Palindromic Substring.py
@@ -1,14 +1,25 @@
 # Given a string s, return the longest  palindromic
 #
 # substring in s.
-
-#
 # Example 1:
 
-# Input: s = "babad"
+# Input: st = "babad"
 # Output: "bab"
 # Explanation: "aba" is also a valid answer.
 # Example 2:
 #
 # Input: s = "cbbd"
 # Output: "bb"
+
+
+def longestPalindomre(st):
+    for idx, ch in range(1,len(st)):
+      print(idx)
+      print(ch)
+
+
+
+
+# st = "abcba"
+st = "abba"
+longestPalindomre(st)
