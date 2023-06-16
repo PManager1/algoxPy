00015a92fd182c02a631f14ce18c8d4300d040df
Jay <jpca999@gmail.com>
2023-06-16 02:42:28 -0400

    -

--- f_Medium/75 Longest Palindromic Substring.py
+++ f_Medium/75 Longest Palindromic Substring.py
@@ -11,13 +11,24 @@
 # Input: s = "cbbd"
 # Output: "bb"
 
+def getPalindrome():
+    print("calling getPalindrome")
+    # Return the difference between the left & right which is the length of the palindrome.
+
 
 def longestPalindomre(st):
-    for idx, ch in range(1,len(st)):
-      print(idx)
-      print(ch)
 
+    # isEven, isOdd
+    if len(st) %2 == 0 : print("is even");  isEven = True #|| print("is even");   # odd = getPalindrome()
+    if len(st) %2 != 0: print("is Odd");  isOdd = True
 
+    currentLongest = [0,1]
+    for i in range(1,len(st)):
+        print("i, st[i]=", i, st[i]);
+        odd = getPalindrome( i-1, i+1 )  # 1,2 | 2,3 | 3,4 | 4,5
+        even = getPalindrome( i-1, i )  # 0,1 | 1,2 | 2,3 | 3,4
+        # Get the difference between the left & right which is the length of the palindrome.
+#         Grab the longest returned palidorme , update the value in currentLongest and return.
 
 
 # st = "abcba"
